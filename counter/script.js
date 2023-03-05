b8cdd725b30b2f52ab884304e5f23b117331a00b
Prathyusha465 <121740500+Prathyusha465@users.noreply.github.com>
2023-03-05 16:31:53 +0530

    Delete counter directory

--- counter/script.js
+++
@@ -1,8 +0,0 @@
- const incEl=document.getElementById("inc");
- const decEl=document.getElementById("dec");
-var count=0;
-function valchange(num)
-{
-     count+=num;
-     document.getElementById("count").innerText=count;
-}
